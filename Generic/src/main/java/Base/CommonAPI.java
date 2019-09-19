9f5da749a7226799cc6fb27689927424535803d7
msarefin <mohammed.arefin@gmail.com>
2019-09-19 12:31:06 -0400

    new Project created for web automation Test

---
+++ Generic/src/main/java/Base/CommonAPI.java
@@ -0,0 +1,14 @@
+package Base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class CommonAPI {
+    public WebDriver driver = null;
+
+    @BeforeMethod
+    public void setup(){
+
+    }
+
+}
